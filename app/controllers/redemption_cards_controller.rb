e25e4b2024939c5ab2c99b7a7f27943fe82f9b10
Stan <stan.amsellem@gmail.com>
2016-04-26 08:35:31 +0200

    create redemption card controller

--- app/controllers/redemption_cards_controller.rb
+++ app/controllers/redemption_cards_controller.rb
@@ -1,2 +1,28 @@
 class RedemptionCardsController < ApplicationController
+
+  # def new
+  #   @redemption_card = RedemptionCard.new
+  # end
+  #
+  # def create
+  #   @redemption_card = RedemptionCard.new(redemption_card_params)
+  #
+  #   if @redemption_card.save
+  #
+  #     redirect_to root_url
+  #   else
+  #     flash.now[:errors] = @redemption_card.errors.full_messages
+  #     render :new
+  #   end
+  # end
+
+  def show
+    @redemption_card = RedemptionCard.find(params[:id])
+  end
+
+  private
+  def redemption_card_params
+    params.require(:redemption_card).permit(:card_code, :card_pin, :amount)
+  end
+
 end
--- app/models/redemption_card.rb
+++ app/models/redemption_card.rb
@@ -1,8 +1,11 @@
 class RedemptionCard < ActiveRecord::Base
 
-validates :card_code, :card_pin, :amount, presence: true
-validates :card_code, uniqueness: true
+  validates :card_code, :card_pin, :amount, presence: true
+  validates :card_code, uniqueness: true
+  validates :card_code, length: { is: 16 }
+  validates :card_pin, length: { is: 4 }
 
-belongs_to :user
+
+  belongs_to :user
 
 end
